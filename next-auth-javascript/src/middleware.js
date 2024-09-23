c69d2c193f6012c6c316e98ae1811164d873e175
uzzal71 <uzzalroy.acm@gmail.com>
2024-09-23 23:22:48 +0600

    create middlware function

---
+++ next-auth-javascript/src/middleware.js
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  console.log("Middleware");
+  return NextResponse.redirect(new URL("/products/1/checkout", request.url));
+}
