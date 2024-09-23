a682e9f80a2f1d24c7c6958bd6306a7c861b0fa0
uzzal71 <uzzalroy.acm@gmail.com>
2024-09-23 23:48:36 +0600

    create me route

---
+++ next-auth-javascript/src/app/api/me/route.js
@@ -0,0 +1,29 @@
+import { auth } from "@/auth";
+import { dbConnect } from "@/lib/mongo";
+import { getUserByEmail } from "@/queries/users";
+
+import { NextResponse } from "next/server";
+
+export const GET = async (request) => {
+  const session = await auth();
+
+  if (!session?.user) {
+    return new NextResponse(`You are not authenticated!`, {
+      status: 500,
+    });
+  }
+
+  await dbConnect();
+
+  try {
+    const user = await getUserByEmail(session?.user?.email);
+    return new NextResponse(JSON.stringify(user), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new NextResponse(error.message, {
+      status: 500,
+    });
+  }
+};
