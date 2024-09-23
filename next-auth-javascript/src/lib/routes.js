f521cc10c5a21e05dd0efbebc0b9ef5d1f755d06
uzzal71 <uzzalroy.acm@gmail.com>
2024-09-23 23:32:23 +0600

    middlware apply public and private routes

---
+++ next-auth-javascript/src/lib/routes.js
@@ -0,0 +1,12 @@
+export const LOGIN = "/login";
+export const ROOT = "/";
+
+export const PUBLIC_ROUTES = [
+  "/login",
+  "/register",
+  "/products",
+  "/api/auth/callback/google",
+  "/api/auth/callback/github",
+];
+
+export const PROTECTED_SUB_ROUTES = ["/checkout"];
--- next-auth-javascript/src/middleware.js
+++ next-auth-javascript/src/middleware.js
@@ -1,10 +1,27 @@
-import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 
-export function middleware(request) {
-  console.log("Middleware");
-  return NextResponse.redirect(new URL("/products/1/checkout", request.url));
+const { auth } = NextAuth(authConfig);
+
+import { LOGIN, PROTECTED_SUB_ROUTES, PUBLIC_ROUTES, ROOT } from "@/lib/routes";
+
+export async function middleware(request) {
+  const { nextUrl } = request;
+  const session = await auth();
+  const isAuthenticated = !!session?.user;
+  console.log(isAuthenticated, nextUrl.pathname);
+
+  const isPublicRoute =
+    (PUBLIC_ROUTES.find((route) => nextUrl.pathname.startsWith(route)) ||
+      nextUrl.pathname === ROOT) &&
+    !PROTECTED_SUB_ROUTES.find((route) => nextUrl.pathname.includes(route));
+
+  console.log(isPublicRoute);
+
+  if (!isAuthenticated && !isPublicRoute)
+    return Response.redirect(new URL(LOGIN, nextUrl));
 }
 
 export const config = {
-  matcher: "/products",
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
